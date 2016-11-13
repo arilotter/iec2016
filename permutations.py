f05813c8165ef13a6ff5662e13e27ac6d72e1bf8
MooseV2 <MooseV2@users.noreply.github.com>
2016-11-13 12:31:06 -0500

    Changes

--- permutations.py
+++ permutations.py
@@ -9,14 +9,33 @@ postroute = it.product(LOC, "END")
 
 full_permutations = [["START"] + list(route) + ["END"] for route in inroute]
 
-print(full_permutations)
+
 def get_optimal_route():
     cheapest = []
     fastest = []
 
-# graph.get_optimal_routes("LOC_1", "LOC_2")
+def cheapest_route():
+    all_routes = []
+    for route in full_permutations:
+        time = 0 #9am
+        cost = 0
+
+        for index in range(len(route)-1):
+            best = graph.get_optimal_routes(route[index], route[index+1])["cheapest"][time//60]
+            cost += best["cost"]
+            time += best["time"] * 20
+
+            #print("From %s to %s (time = %s, cost = %s): " % (route[index], route[index+1], time//60, cost))
+        all_routes.append({"cost": cost, "time": time, "path": route})
 
 
+    #best_route = sorted(all_routes, key=lambda x:x['cost'])[0]
+    return cheapest_route()
+
+
+
+
+cheapest_route()
 
 
 
