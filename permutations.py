ccb56b8ca91d7be1fbb9906f0533060db93e0fdb
MooseV2 <MooseV2@users.noreply.github.com>
2016-11-13 12:34:29 -0500

    Changes

--- permutations.py
+++ permutations.py
@@ -27,7 +27,26 @@ def get_fastest_route():
 
 
 
-get_optimal_route()
+def cheapest_route():
+    all_routes = []
+    for route in full_permutations:
+        time = 0 #9am
+        cost = 0
+
+        for index in range(len(route)-1):
+            best = graph.get_optimal_routes(route[index], route[index+1])["cheapest"][time//60]
+            cost += best["cost"]
+            time += best["time"] * 20
+
+            #print("From %s to %s (time = %s, cost = %s): " % (route[index], route[index+1], time//60, cost))
+        all_routes.append({"cost": cost, "time": time, "path": route})
+
+
+    #best_route = sorted(all_routes, key=lambda x:x['cost'])[0]
+    return cheapest_route()
+
+
+
 
 
 
