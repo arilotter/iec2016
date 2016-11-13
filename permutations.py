31426da9472ed17a34b3884885eb906569748766
arilotter <arilotter@gmail.com>
2016-11-13 12:30:58 -0500

    fastest

--- permutations.py
+++ permutations.py
@@ -9,16 +9,25 @@ postroute = it.product(LOC, "END")
 
 full_permutations = [["START"] + list(route) + ["END"] for route in inroute]
 
-print(full_permutations)
 def get_optimal_route():
     cheapest = []
-    fastest = []
+    fastest = get_fastest_route()
 
+def get_fastest_route():
+    perm_routes = []
+    for perm in full_permutations:
+        route = []
+        for index, node in enumerate(perm):
+            if index > 0:
+                route += graph.get_optimal_routes(perm[index - 1], node)["fastest"][:-1]
+        perm_routes.append(route)
+    fastest = sorted(perm_routes, key=graph.get_time)[0] + ['END']
+    return fastest
 # graph.get_optimal_routes("LOC_1", "LOC_2")
 
 
 
-
+get_optimal_route()
 
 
 
