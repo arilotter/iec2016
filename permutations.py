99d4ed9cc2a83834f6ebf07d1c9fb6f62060efa6
arilotter <arilotter@gmail.com>
2016-11-13 12:04:45 -0500

    full permutations

--- permutations.py
+++ permutations.py
@@ -7,15 +7,14 @@ inroute = it.permutations(LOC, 4)
 preroute = it.product(["START"], LOC)
 postroute = it.product(LOC, "END")
 
+full_permutations = [["START"] + list(route) + ["END"] for route in inroute]
+
+print(full_permutations)
 def get_optimal_route():
     cheapest = []
     fastest = []
 
-
-
-
-
-graph.get_optimal_routes("LOC_1", "LOC_2")
+# graph.get_optimal_routes("LOC_1", "LOC_2")
 
 
 
